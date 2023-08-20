8912d8cca3fedefe805f133dda6c743a4c5d805f
Trevor Settles <roboteng@gmail.com>
2023-08-20 04:50:36 -0600

    feat: ran `cargo check` in ci

--- ci/src/main.rs
+++ ci/src/main.rs
@@ -1,15 +1,29 @@
+use dagger_sdk::HostDirectoryOpts;
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let client = dagger_sdk::connect().await?;
 
-    let version = client
+    let host_source_dir = client.host().directory_opts(
+        ".",
+        HostDirectoryOpts {
+            exclude: Some(vec!["target".into()]),
+            include: None,
+        },
+    );
+
+    let container = client
         .container()
         .from("rust:1.71.1")
-        .with_exec(vec!["cargo", "version"])
+        .with_mounted_directory("/src", host_source_dir.id().await?);
+
+    let check = container
+        .with_workdir("/src")
+        .with_exec(vec!["cargo", "check"])
         .stdout()
         .await?;
 
-    println!("Hello from Dagger and {}", version.trim());
+    println!("Hello from Dagger and {}", check.trim());
 
     Ok(())
 }
