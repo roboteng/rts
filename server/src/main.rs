6e9c20f31b53b6b93a2dcb5dbbc74412d9e3f726
Trevor Settles <roboteng@gmail.com>
2023-10-05 14:37:29 -0600

    feat: started change players

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,28 +4,62 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub struct Server;
+pub struct Server {
+    connection: Option<Connection>,
+}
+
 impl Server {
     pub fn new() -> Self {
-        Self
+        Self { connection: None }
     }
 
     pub fn add(&mut self, conn: Connection) {
-        conn.0.send(Message::Host).unwrap();
+        conn.reply_tx.send(Message::JoinedAsHost).unwrap();
+        self.connection = Some(conn);
+    }
+
+    fn tick(&mut self) {
+        self.connection
+            .as_mut()
+            .map(|s| s.reply_tx.send(Message::ChangePlayers(3)).unwrap());
     }
 }
 
-pub struct Connection(Sender<Message>);
+pub struct ClientConnection {
+    tx: Sender<ServerRequest>,
+    reply_rx: Receiver<Message>,
+}
+
+pub struct Connection {
+    reqest_rx: Receiver<ServerRequest>,
+    reply_tx: Sender<Message>,
+}
 impl Connection {
-    pub fn new() -> (Self, Receiver<Message>) {
-        let (tx, rx) = channel();
-        (Self(tx), rx)
+    pub fn new() -> (Self, ClientConnection) {
+        let (request_tx, reqest_rx) = channel();
+        let (reply_tx, reply_rx) = channel();
+
+        (
+            Self {
+                reply_tx,
+                reqest_rx,
+            },
+            ClientConnection {
+                reply_rx,
+                tx: request_tx,
+            },
+        )
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Message {
-    Host,
+    JoinedAsHost,
+    ChangePlayers(u8),
+}
+
+pub enum ServerRequest {
+    ChangePlayers(u8),
 }
 
 #[cfg(test)]
@@ -36,14 +70,31 @@ mod test {
     /// Given the server is running
     /// When Alice joins
     /// Then Alice is the Host
-    // /// And Alice is able to change the numbers of players to 3
     fn host_joins() {
         let mut server = Server::new();
         let (conn, reply) = Connection::new();
 
         server.add(conn);
-        let response = reply.recv().unwrap();
+        let response = reply.reply_rx.recv().unwrap();
 
-        assert_eq!(response, Message::Host);
+        assert_eq!(response, Message::JoinedAsHost);
+    }
+
+    #[test]
+    /// Given the server is running
+    /// And Alice is the host
+    /// When Alice tries to change the number of players to 3
+    /// Then Alice is able to change the numbers of players to 3
+    fn host_changes_number_of_players() {
+        let mut server = Server::new();
+        let (conn, reply) = Connection::new();
+        server.add(conn);
+        let _ = reply.reply_rx.recv().unwrap();
+
+        reply.tx.send(ServerRequest::ChangePlayers(3)).unwrap();
+        server.tick();
+
+        let response = reply.reply_rx.recv().unwrap();
+        assert_eq!(response, Message::ChangePlayers(3));
     }
 }
