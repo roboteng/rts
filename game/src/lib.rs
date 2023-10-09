2f19742e1b2d4a26cb27963dfd76532e2a6f62ce
Trevor Settles <roboteng@gmail.com>
2023-10-09 01:04:00 -0600

    feat: started basic ui

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -12,10 +12,33 @@ impl Plugin for BasePlugin {
 fn draw_main_menu(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(TextBundle::from_section(
-        "Hello, World!",
-        TextStyle::default(),
-    ));
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .with_children(|builder| {
+            builder.spawn(TextBundle::from_section(
+                "Hello, World!",
+                TextStyle::default(),
+            ));
+            builder
+                .spawn(ButtonBundle {
+                    ..Default::default()
+                })
+                .with_children(|builder| {
+                    builder.spawn(TextBundle::from_section(
+                        "Hello, Button!",
+                        TextStyle {
+                            color: Color::BLACK,
+                            ..Default::default()
+                        },
+                    ));
+                });
+        });
 }
 
 #[derive(Debug, Default, Hash, PartialEq, Eq, Clone, States)]
