61ffdba09bd6491fd2dc1352595421e6d1629641
Trevor Settles <roboteng@gmail.com>
2023-10-13 15:37:25 -0600

    feat: main menu teardown

--- game/src/main_menu.rs
+++ game/src/main_menu.rs
@@ -10,7 +10,8 @@ impl Plugin for MainMenuPlugin {
             .add_systems(
                 Update,
                 (button_interaction, button_click).run_if(in_state(GameState::MainMenu)),
-            );
+            )
+            .add_systems(OnExit(GameState::MainMenu), teardown);
     }
 }
 
@@ -52,6 +53,9 @@ fn c_title_test(_: &AssetServer, a: &mut TextStyle) {
 }
 
 #[derive(Component)]
+struct OnMenuScreen;
+
+#[derive(Component)]
 enum MyButton {
     SinglePlayer,
     MultiPlayer,
@@ -60,24 +64,30 @@ enum MyButton {
 }
 
 fn draw_main_menu(mut commands: Commands, assets: Res<AssetServer>) {
-    root((c_background, c_centers), &assets, &mut commands, |p| {
-        node(c_main_menu, p, |p| {
-            text("The Tales our\nAncestors Told", (), c_title_test, p);
+    rooti(
+        (c_background, c_centers),
+        &assets,
+        &mut commands,
+        OnMenuScreen,
+        |p| {
+            node(c_main_menu, p, |p| {
+                text("The Tales our\nAncestors Told", (), c_title_test, p);
 
-            buttoni(c_button, MyButton::SinglePlayer, p, |p| {
-                text("Single Player", (), c_button_text, p);
+                buttoni(c_button, MyButton::SinglePlayer, p, |p| {
+                    text("Single Player", (), c_button_text, p);
+                });
+                buttoni(c_button, MyButton::MultiPlayer, p, |p| {
+                    text("Multi Player", (), c_button_text, p);
+                });
+                buttoni(c_button, MyButton::Settings, p, |p| {
+                    text("Settings", (), c_button_text, p);
+                });
+                buttoni(c_button, MyButton::Quit, p, |p| {
+                    text("Quit", (), c_button_text, p);
+                });
             });
-            buttoni(c_button, MyButton::MultiPlayer, p, |p| {
-                text("Multi Player", (), c_button_text, p);
-            });
-            buttoni(c_button, MyButton::Settings, p, |p| {
-                text("Settings", (), c_button_text, p);
-            });
-            buttoni(c_button, MyButton::Quit, p, |p| {
-                text("Quit", (), c_button_text, p);
-            });
-        });
-    });
+        },
+    );
 }
 
 fn button_interaction(
@@ -98,12 +108,23 @@ fn button_interaction(
 fn button_click(
     query: Query<(&Interaction, &MyButton), Changed<Interaction>>,
     mut exit_event: ResMut<Events<AppExit>>,
+    mut states: ResMut<NextState<GameState>>,
 ) {
     for (interaction, button) in &query {
-        if let MyButton::Quit = button {
-            if let Interaction::Pressed = interaction {
-                exit_event.send(AppExit)
+        if let Interaction::Pressed = interaction {
+            match button {
+                MyButton::Quit => exit_event.send(AppExit),
+                MyButton::Settings => {
+                    states.set(GameState::Settings);
+                }
+                _ => {}
             }
         }
+    }
+}
+
+fn teardown(mut commands: Commands, q: Query<Entity, With<OnMenuScreen>>) {
+    for e in &q {
+        commands.entity(e).despawn_recursive();
     }
 }
