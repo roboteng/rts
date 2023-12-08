c0140cb54ba278c42001560197fcbceb2e153dd8
Trevor Settles <roboteng@gmail.com>
2023-12-07 21:46:41 -0700

    dev: update test to run on virtual time

--- src/lib.rs
+++ src/lib.rs
@@ -33,27 +33,45 @@ enum PlayerCommand {
 #[cfg(test)]
 mod test {
     use super::*;
+    mod acceptance {
+        use std::time::Duration;
 
-    #[test]
-    fn move_a_villager() {
-        // arrange
-        let mut app = App::new();
+        use super::*;
 
-        app.add_plugins((MinimalPlugins, CoreLogicPlugin));
+        #[test]
+        fn move_a_villager() {
+            // arrange
+            let mut app = App::new();
 
-        let ent = app.world.spawn_empty().id();
+            app.add_plugins((MinimalPlugins, CoreLogicPlugin));
 
-        let init_pos = Vec2::new(1.0, 1.0);
-        app.world.send_event(SpawnUnit::Villager(ent, init_pos));
+            let ent = app.world.spawn_empty().id();
 
-        // act
-        let goal_pos = Vec2::new(0.0, 0.0);
-        app.world.send_event(PlayerCommand::Move(ent, goal_pos));
-        app.update();
+            let init_pos = Vec2::new(1.0, 1.0);
+            app.world.send_event(SpawnUnit::Villager(ent, init_pos));
 
-        // assert
-        // Within 5 seconds, the villager should be closer than where it started
-        let actual = app.world.get::<Transform>(ent).unwrap();
-        assert!(init_pos.length() > actual.translation.length());
+            // act
+            let goal_pos = Vec2::new(0.0, 0.0);
+            app.world.send_event(PlayerCommand::Move(ent, goal_pos));
+            app.update();
+
+            // assert
+            loop {
+                {
+                    let actual = app.world.get::<Transform>(ent).unwrap();
+                    if actual.translation.length() < init_pos.length() {
+                        break;
+                    }
+                }
+
+                let mut time = app.world.get_resource_mut::<Time<Virtual>>();
+                let time = time.as_mut().unwrap();
+                time.advance_by(Duration::from_millis(16));
+                if time.elapsed_seconds() > 5.0 {
+                    panic!("Villager didn't move closer to the goal")
+                }
+                app.update();
+            }
+        }
     }
 }
