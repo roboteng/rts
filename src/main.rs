ec21a360ed13effad9dab7e25526f33d9843b248
Trevor Settles <roboteng@gmail.com>
2024-01-12 19:46:55 -0700

    dev: created plugin for visuals

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,71 @@ use bevy::{
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 use rts::{CoreLogicPlugin, MoveToCommand, SpawnUnit, SpawnUnitData, Speed, Vec3Extension};
+use visuals::VisualsPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, CoreLogicPlugin))
+        .add_plugins((DefaultPlugins, CoreLogicPlugin, VisualsPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, (create, move_unit, select_unit, show_selected))
+        .add_systems(Update, (move_unit, select_unit, show_selected))
         .run();
+}
+
+mod visuals {
+    use bevy::{
+        prelude::*,
+        sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+    };
+    use rts::*;
+
+    pub struct VisualsPlugin;
+    impl Plugin for VisualsPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_systems(Startup, setup).add_systems(Update, create);
+        }
+    }
+
+    fn setup(mut commands: Commands) {
+        commands.spawn(Camera2dBundle {
+            ..Default::default()
+        });
+    }
+
+    fn create(
+        mut events: EventReader<SpawnUnit>,
+        transforms: Query<&Transform>,
+        mut commands: Commands,
+        mut materials: ResMut<Assets<ColorMaterial>>,
+        mut meshes: ResMut<Assets<Mesh>>,
+    ) {
+        // Build a default quad mesh
+        let mut mesh = Mesh::from(shape::Quad::default());
+        // Build vertex colors for the quad. One entry per vertex (the corners of the quad)
+        let vertex_colors: Vec<[f32; 4]> = vec![
+            Color::RED.as_rgba_f32(),
+            Color::GREEN.as_rgba_f32(),
+            Color::BLUE.as_rgba_f32(),
+            Color::WHITE.as_rgba_f32(),
+        ];
+        // Insert the vertex colors as an attribute
+        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
+
+        let mesh_handle: Mesh2dHandle = meshes.add(mesh).into();
+
+        for event in events.read() {
+            if let Some(mut ent) = commands.get_entity(event.target) {
+                ent.insert(MaterialMesh2dBundle {
+                    mesh: mesh_handle.clone(),
+                    transform: transforms
+                        .get(event.target)
+                        .unwrap_or(&Transform::default())
+                        .with_scale(Vec3::splat(128.)),
+                    material: materials.add(ColorMaterial::default()),
+                    ..default()
+                });
+            }
+        }
+    }
 }
 
 fn setup(mut s: EventWriter<SpawnUnit>, mut commands: Commands) {
@@ -20,46 +78,6 @@ fn setup(mut s: EventWriter<SpawnUnit>, mut commands: Commands) {
             pos: Vec2::new(0.0, 0.0),
         },
     });
-
-    commands.spawn(Camera2dBundle {
-        ..Default::default()
-    });
-}
-
-fn create(
-    mut events: EventReader<SpawnUnit>,
-    transforms: Query<&Transform>,
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-    // Build a default quad mesh
-    let mut mesh = Mesh::from(shape::Quad::default());
-    // Build vertex colors for the quad. One entry per vertex (the corners of the quad)
-    let vertex_colors: Vec<[f32; 4]> = vec![
-        Color::RED.as_rgba_f32(),
-        Color::GREEN.as_rgba_f32(),
-        Color::BLUE.as_rgba_f32(),
-        Color::WHITE.as_rgba_f32(),
-    ];
-    // Insert the vertex colors as an attribute
-    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
-
-    let mesh_handle: Mesh2dHandle = meshes.add(mesh).into();
-
-    for event in events.read() {
-        if let Some(mut ent) = commands.get_entity(event.target) {
-            ent.insert(MaterialMesh2dBundle {
-                mesh: mesh_handle.clone(),
-                transform: transforms
-                    .get(event.target)
-                    .unwrap_or(&Transform::default())
-                    .with_scale(Vec3::splat(128.)),
-                material: materials.add(ColorMaterial::default()),
-                ..default()
-            });
-        }
-    }
 }
 
 fn move_unit(
